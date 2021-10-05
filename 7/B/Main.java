a90bc6013faeb5666199e9e8250a03a365803625
LeonardoFG20 <69605104+LeonardoFG20@users.noreply.github.com>
2021-10-04 23:30:23 -0300

    Create Main.java

---
+++ 7/B/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("teste.txt"));
+			System.out.println(in.readLine());
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
