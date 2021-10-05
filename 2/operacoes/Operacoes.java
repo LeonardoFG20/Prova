dad85095f0b28c6de0826e57f994cdcb66eee9b8
LeonardoFG20 <69605104+LeonardoFG20@users.noreply.github.com>
2021-10-04 22:49:04 -0300

    Create Operacoes.java

---
+++ 2/operacoes/Operacoes.java
@@ -0,0 +1,24 @@
+package operacoes;
+
+public class Operacoes{
+
+	public Integer n1 = 10;
+	public Integer n2 = 10;
+
+	public int somar() {
+		return n1 + n2;
+	}
+
+	public int sub() {
+		return n1 - n2;
+	}
+
+	public int divi() {
+		return n1 / n2;
+	}
+
+	public int mult() {
+		return n1 * n2;
+	}
+
+}
