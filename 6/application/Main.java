4d46fbd91cfc820ec2549f7a63faa92d29f94e03
LeonardoFG20 <69605104+LeonardoFG20@users.noreply.github.com>
2021-10-04 23:27:05 -0300

    Create Main.java

---
+++ 6/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("prova.txt")));
+			out.write("Leonardo Franco Gambeta");
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("teste.txt"));
+			System.out.println(in.readLine());
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
